d926e3bf785f68258a7ceae0e785b3e215e431c6
Karol Wojdyła <karol.wojdyla@gmail.com>
2020-02-26 20:37:19 +0100

    Refactor upload file

--- upload.go
+++ upload.go
@@ -1,33 +1,35 @@
 package main
 
 import (
-	. "fmt"
-	"github.com/julienschmidt/httprouter"
+	"fmt"
 	"net/http"
-)
 
+	"github.com/julienschmidt/httprouter"
+)
 
 // UploadFile uploads a file to the server
 func UploadFile(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	file, handle, err := r.FormFile(FileField)
-	if file == nil {
-		Fprint(w, STRINGS["noFile"])
-		return
+	enableCors(&w)
+
+	r.ParseMultipartForm(32 << 20) // 32MB is the default used by FormFile
+	files := r.MultipartForm.File["files"]
+	for _, file := range files {
+		f, err := file.Open()
+
+		if err != nil {
+			fmt.Fprintf(w, "%v", err)
+			return
+		}
+		defer f.Close()
+
+		mimeType := file.Header.Get("Content-Type")
+		switch mimeType {
+		case "image/jpeg":
+		case "image/png":
+			saveFile(w, f, file)
+		default:
+			jsonResponse(w, http.StatusBadRequest, STRINGS["fileFormatInvalid"])
+		}
 	}
 
-	if err != nil {
-		Fprintf(w, "%v", err)
-		return
-	}
-	defer file.Close()
-
-	mimeType := handle.Header.Get("Content-Type")
-	switch mimeType {
-	case "image/jpeg":
-		saveFile(w, file, handle)
-	case "image/png":
-		saveFile(w, file, handle)
-	default:
-		jsonResponse(w, http.StatusBadRequest, STRINGS["fileFormatInvalid"])
-	}
 }
