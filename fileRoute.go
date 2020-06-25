64d9ea7dbcd25e7e652518afa32188166e822c93
Karol Wojdyła <karol.wojdyla@gmail.com>
2020-06-25 22:45:06 +0200

    Refactor fileRoute

--- fileRoute.go
+++ fileRoute.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	appDB "photos/db"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/rs/zerolog/log"
 )
 
 func deleteFileRoute(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -19,12 +19,22 @@ func deleteFileRoute(w http.ResponseWriter, r *http.Request, _ httprouter.Params
 	err := json.NewDecoder(r.Body).Decode(&payload)
 
 	if err != nil {
-		panic(err)
+		log.Error().Err(err).Caller().Int("user", userID).Msg("Can't parse files' id to delete")
+
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
-	err = appDB.DeleteFiles(payload.ID, userID, db)
-	if err != nil {
-		fmt.Println("deleteFile", err)
+	notDeleted := appDB.DeleteFiles(payload.ID, userID, db)
+	if len(notDeleted) > 0 {
+		response, err := json.Marshal(Payload{notDeleted})
+		if err != nil {
+			log.Error().Err(err).Caller().Int("user", userID).Msg("Can't parse not deleted files")
+			response, _ = json.Marshal(Payload{})
+		}
+
+		jsonResponse(w, http.StatusUnprocessableEntity, string(response))
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
@@ -44,9 +54,9 @@ func fetchFilesRoute(w http.ResponseWriter, r *http.Request, _ httprouter.Params
 	enableCors(&w)
 	files, err := appDB.GetFiles(userID, db)
 	if err != nil {
-		fmt.Println("fetchFilesRoute", err)
-		w.WriteHeader(http.StatusInternalServerError)
+		log.Error().Err(err).Caller().Int("user", userID).Msg("Can't parse files")
 
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
